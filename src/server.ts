4506b468003af0b9bd30ec02f1bc53694791b400
Liam Fitzgerald <liam@urizen.co>
2020-01-13 17:30:29 +1100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,193 @@
+import * as rpc from "vscode-jsonrpc";
+import { NotificationMessage, RequestMessage } from "vscode-languageserver";
+import * as pino from "pino";
+import { uniqueId } from "lodash";
+import * as qs from "querystring";
+import { wait } from "./util";
+
+import { request } from "./util";
+import Channel from "./channel";
+
+const logger = pino(
+  { level: "trace" },
+  pino.destination("/tmp/hoon-language-server.log")
+);
+const url = "http://localhost";
+const app = "language-server";
+const path = "/primary";
+const ship = "zod";
+
+enum MARKS {
+  REQUEST = "language-server-rpc-request",
+  NOTIFICATION = "language-server-rpc-notification",
+  RESPONSE = "language-server-rpc-response"
+};
+
+interface RequestContinuation {
+  (result: any): void
+}
+
+class Server {
+  connection: rpc.MessageConnection;
+  subscription: number | null = null;
+  outstandingRequests: Map<string, RequestContinuation> = new Map();
+  constructor(private channel: Channel) {
+    this.connection = rpc.createMessageConnection(
+      new rpc.StreamMessageReader(process.stdin),
+      new rpc.StreamMessageWriter(process.stdout)
+    );
+    // this.connection.trace(rpc.Trace.Messages, tracer);
+
+    this.connection.onNotification((method, params) =>
+      this.onNotification(method, params)
+    );
+    this.connection.onRequest((method, params) =>
+      this.onRequest(method, params)
+    );
+  }
+
+  initialise() {
+    return {
+      capabilities: {
+        hoverProvider: true,
+        textDocumentSync: {
+          openClose: true,
+          change: 2,
+          willSave: true,
+          save: true
+        },
+        definitionProvider: true,
+        completionProvider: {
+          resolveProvider: false,
+          triggerCharacters: [
+            "-",
+            "+",
+            "^",
+            "!",
+            "@",
+            "$",
+            "%",
+            ".",
+            "%",
+            "&",
+            "*",
+            "/",
+            ",",
+            ">",
+            "<",
+            "~",
+            "|",
+            "=",
+            ";",
+            ":",
+            "_"
+          ]
+        },
+        workspace: {
+          workspaceFolders: {
+            supported: true,
+            changeNotifications: true
+          }
+        }
+      }
+    };
+  }
+
+  serve() {
+    // const onUpdate =
+    // const onError = this.onSubscriptionErr.bind(this);
+    this.channel.subscribe(
+      ship,
+      app,
+      path,
+      e => this.onSubscriptionErr(e),
+      u => this.onSubscriptionUpdate(u),
+      e => this.onSubscriptionErr(e)
+    );
+    new Promise(() => this.connection.listen());
+  }
+
+  // handlers
+
+  onNotification(method: string, params: any[]) {
+    logger.debug({ method, params });
+    if (method === "textDocument/didSave") {
+      wait(1000).then(() => {
+        logger.debug('Delayed didSave')
+        this.poke(MARKS.NOTIFICATION, { jsonrpc: "2.0", method, params });
+      });
+    }
+    this.poke(MARKS.NOTIFICATION, { jsonrpc: "2.0", method, params });
+  }
+
+  onRequest(method: string, params: any[]) {
+    const id = uniqueId();
+    logger.debug({ method });
+
+    if (method === "initialize") {
+      return this.initialise();
+    } else {
+      logger.info({ message: `caught request`, id })
+      this.poke(MARKS.REQUEST, { jsonrpc: '2.0', method, params, id })
+      return this.waitOnResponse(id).then(r => {
+        logger.info({ message: `returning request`, id, r })
+        return r;
+
+      });
+    }
+  }
+
+  waitOnResponse(id: string) {
+    return new Promise((resolve, reject) => {
+      this.outstandingRequests.set(id, resolve);
+    });
+  }
+
+  poke(mark: MARKS, message: NotificationMessage | RequestMessage) {
+    return new Promise((resolve, reject) =>
+      this.channel.poke(ship, app, mark, message, resolve, reject)
+    ).catch(e => {
+      logger.warn("Failed to poke", { error: e})
+    });
+  }
+
+  // Subscriptions
+
+  onSubscriptionUpdate(update: any) {
+    logger.info({ message: "Subscription update", update });
+
+    if (update.hasOwnProperty("id")) {
+      const response = this.outstandingRequests.get(update.id);
+      if(response) {
+        response(update.result)
+      } else {
+        logger.warn("Unrecognised request", { update })
+      }
+
+    } else {
+      this.connection.sendNotification(update.method, update.params);
+    }
+  }
+
+  onSubscriptionErr(e: any) {
+    logger.fatal({ message: "Subscription Errored", e });
+  }
+
+  onSubscriptionQuit(e: any) {
+    logger.fatal({ message: "Subscription Quit", e });
+  }
+}
+
+let data = { password: "lidlut-tabwed-pillex-ridrup" };
+request(url + "/~/login", { method: "post" }, qs.stringify(data)).then(
+  ({ res }) => {
+    logger.info({
+      message: "Got cookie",
+      cookie: res.headers["set-cookie"] || "no-cookie"
+    });
+    const channel = new Channel(res.headers["set-cookie"] || [], url, logger);
+    const server = new Server(channel);
+
+    server.serve();
+  }
+);
---
+++ src/util.ts
@@ -0,0 +1,37 @@
+import * as http from "http";
+
+interface HttpResponse {
+  req: http.ClientRequest;
+  res: http.IncomingMessage;
+  data: string;
+}
+export function request(
+  url: string,
+  options: http.ClientRequestArgs,
+  body?: string
+) {
+  return new Promise<HttpResponse>((resolve, reject) => {
+    const req = http.request(url, options, res => {
+      let data = "";
+      res.on("data", chunk => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        resolve({ req, res, data });
+      });
+      res.on("error", e => {
+        reject(e);
+      });
+    });
+    if (body) {
+      req.write(body);
+    }
+    req.end();
+  });
+}
+
+export function wait(time: number) {
+  return new Promise((resolve) =>
+    setTimeout(resolve, time)
+  )
+}
